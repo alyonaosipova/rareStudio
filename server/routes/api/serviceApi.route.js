380e88c689796273fe84620b926ddba13c58d549
Vladislav-Ovsyannikov <vladikslav25@gmail.com>
2024-04-14 22:48:36 +0300

    Create logOutroute,checkoutRoute,serviceApiRoute

--- server/routes/api/serviceApi.route.js
+++ server/routes/api/serviceApi.route.js
@@ -1,10 +1,21 @@
 const router = require('express').Router()
 const {Service} = require('../../db/models/service')
+async function txt(){
+const services = await Service.findAll()
+console.log(services);
+}
+router.get('/user/services', async (req, res) => {
+    const services = await Service.findAll({
+        attributes:[title, description, price]
+    })
+    res.json({services})
+})
 
-router.get('/user', async (req, res) => {
+router.get('/admin/services', async (req, res) => {
     const services = await Service.findAll()
     res.json({services})
 })
 
-router.post('/')
+router
+
 module.exports = router
