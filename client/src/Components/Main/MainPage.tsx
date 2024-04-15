6b8868a7b47d72058d07a85837f2c39f5500f423
alyonaosipova <alyonaosipova@gmail.com>
2024-04-15 14:57:54 +0300

    carousel

--- client/src/Components/Main/MainPage.tsx
+++ client/src/Components/Main/MainPage.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
+import Carousel from './Carousel';
 
 function MainPage(): JSX.Element {
-  return <ReactAudioPlayer src="08. OG Buda feat. LIL KRYSTALLL - Тупой.mp3" autoPlay controls />;
+  return (
+    <>
+      <div>logo</div>
+      <div>студия звукозаписи</div>
+      <div>Rare Studio (м. Площадь Восстания) [spb]</div>
+      <button type="button">Забронировать время</button>
+
+      <Carousel />
+      <br />
+      <br />
+      <br />
+
+      <ReactAudioPlayer src="08. OG Buda feat. LIL KRYSTALLL - Тупой.mp3" autoPlay controls />
+      <br />
+      <ReactAudioPlayer src="08. OG Buda feat. LIL KRYSTALLL - Тупой.mp3" autoPlay controls />
+      <br />
+      <ReactAudioPlayer src="08. OG Buda feat. LIL KRYSTALLL - Тупой.mp3" autoPlay controls />
+      <br />
+      <br />
+
+      <h3>
+        С нами работают: Brick Bazuka, Грязный Луи, OG BUDA, MAYOT, SEEMEE, 163 ONMYNECK,
+        FriendlyThug52, Glocki52 , KOUT, ICEGERGERT, TikoTheCEO, RIGOS, Крип-а-крип, GOKILLA, Слава
+        КПСС, MankyMonk(ОУ74) и другие.
+      </h3>
+      <br />
+      <br />
+      <div className="photos-container">
+        <img className="photos" src="IMG_8973.jpg" alt="Image 8" />
+        <img className="photos" src="IMG_8974.jpg" alt="Image 9" />
+        <img className="photos" src="IMG_8976.jpg" alt="Image 10" />
+        <img className="photos" src="IMG_8979.jpg" alt="Image 11" />
+
+        <img className="photos" src="IMG_9144.jpg" alt="Image 12" />
+        <img className="photos" src="IMG_9270.jpg" alt="Image 13" />
+        <img className="photos" src="IMG_9259.jpg" alt="Image 14" />
+        <img className="photos" src="IMG_9274.jpg" alt="Image 15" />
+        <img className="photos" src="IMG_9303.jpg" alt="Image 15" />
+        <img className="photos" src="IMG_9335.jpg" alt="Image 15" />
+      </div>
+    </>
+  );
 }
 
 export default MainPage;
--- client/src/Components/Services/ServicesPage.tsx
+++ client/src/Components/Services/ServicesPage.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useAppDispatch } from '../../redux/store';
+import ServiceCard from './serviceCard';
 
 function ServicesPage(): JSX.Element {
-  return <div>SERVICES</div>;
+  const dispatch = useAppDispatch();
+
+  const services = useSelector((store: RootState) => store.services);
+
+  return (
+    <div>
+      <h1>УСЛУГИ RARE STUDIO</h1>
+      {/* <ServiceAdd  /> */}
+      <div className="list">
+        SERVICES
+        {/* {services.map((service) => (
+          <ServiceCard service={service} key={service.id} />
+        ))} */}
+      </div>
+    </div>
+  );
 }
 
 export default ServicesPage;
