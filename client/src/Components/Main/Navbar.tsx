76b0ef9de7d0f4dffe07a69ce1b7027030686f72
alyonaosipova <alyonaosipova@gmail.com>
2024-04-15 20:43:00 +0300

    editNavbar

--- client/src/Components/Main/Navbar.tsx
+++ client/src/Components/Main/Navbar.tsx
@@ -4,33 +4,40 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../redux/store';
 import { useAppDispatch } from '../../redux/store';
-import {authLogout} from '../Auth/AuthSlice'
+import { authLogout } from '../Auth/AuthSlice';
 
 function Navbar({ theme, toggleTheme }: { theme: string; toggleTheme: () => void }): JSX.Element {
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const user = useSelector((store: RootState) => store.user.user);
 
-
   async function logout(): Promise<void> {
-    dispatch(authLogout()).catch(console.log)
-    navigate('/')
-}
+    dispatch(authLogout()).catch(console.log);
+    navigate('/');
+  }
   return (
     <div className="navbar-container">
       <nav className="navbar">
         <Link className="navbar-link" to="/">
           Main
         </Link>
-        {user && <div className="navbar-link">Привет, {user?.name}</div>}
-       { !user && <> <Link className="navbar-link" to="/registration">
-          Registration
+        <Link className="navbar-link" to="/services">
+          Services
         </Link>
-        <Link className="navbar-link" to="/login">
-          Autarization
-        </Link> </>}
-        <Link className="navbar-link" to= '/'onClick={() => logout()}>
-            <div>Logout</div>
+        {user && <div className="navbar-link">Привет, {user?.name}</div>}
+        {!user && (
+          <>
+            {' '}
+            <Link className="navbar-link" to="/registration">
+              Registration
+            </Link>
+            <Link className="navbar-link" to="/login">
+              Autarization
+            </Link>{' '}
+          </>
+        )}
+        <Link className="navbar-link" to="/" onClick={() => logout()}>
+          <div>Logout</div>
         </Link>
         <button className={` switchBtn ${theme}`} type="button" onClick={toggleTheme}>
           Switch theme
